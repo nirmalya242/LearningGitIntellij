97a8907d7c902758110c942a251e1dad7c4836ff
NirmalyaBharati <101939589+NirmalyaBharati@users.noreply.github.com>
2022-03-19 21:34:06 +0530

    added user name

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -1,7 +1,7 @@
 public class DevClass {
 
     public static void main(String[] args) {
-        System.out.println("DevClass");
+        System.out.println("DevClass"+"Nirmalya071993");
         //Added by Nirmalya after forking and also collaborative coding
     }
 }
