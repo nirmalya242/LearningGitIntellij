fc60c0881eefa0c8b48358d017bd0561298ddb79
NirmalyaBharati <101939589+NirmalyaBharati@users.noreply.github.com>
2022-03-19 21:10:59 +0530

    Update DevClass.java

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -2,6 +2,6 @@ public class DevClass {
 
     public static void main(String[] args) {
         System.out.println("DevClass");
-        //Added by Nirmalya
+        //Added by Nirmalya after forking and also collaborative coding
     }
 }
