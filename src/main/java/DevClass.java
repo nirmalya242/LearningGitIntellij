bfdc66fe32fd4f247802fe8c264383d5d93f9879
NirmalyaBharati <101939589+NirmalyaBharati@users.noreply.github.com>
2022-03-20 14:54:06 +0530

    adding for feature/feature2 (#3)

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -3,5 +3,6 @@ public class DevClass {
     public static void main(String[] args) {
         System.out.println("DevClass"+"Nirmalya071993");
         //Added by Nirmalya after forking and also collaborative coding
+        //added by nirmalya bharati for feature/feature2
     }
 }
