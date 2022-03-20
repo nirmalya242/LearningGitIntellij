197a5c1fd7d6370a899bea82cf4d685541c4e416
Nirmalya242 <nirmalya242@gmail.com>
2022-03-20 14:45:08 +0530

    added from feature1

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -3,5 +3,6 @@ public class DevClass {
     public static void main(String[] args) {
         System.out.println("DevClass");
         //Added by Nirmalya
+        //this part is commited by feature/feature1
     }
 }
